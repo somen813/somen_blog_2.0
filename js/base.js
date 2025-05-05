94deb7c71195783757908569ebdbae7ccd99b793
somen813 <153832603+somen813@users.noreply.github.com>
2025-05-05 20:19:37 +0900

    [script]base.jsからページトップボタン関数を削除

--- js/base.js
+++ js/base.js
@@ -1,5 +1,8 @@
+//SNSシェアボタン,ページトップボタンにバグあり。要修正
+
+
+
 const nowURL = window.location.href;
-//headerのスクリプト
 const content = document.querySelector('.content');
 const contentHeader = () => {
 	fetch('/common-tags/header.html')
@@ -105,88 +108,3 @@ for(let i = 0; i < refinement.length; i++){
 		};
 	}
 }
-console.log(article.slice().filter(item => item.categories === 'その他'));
-console.log(article.slice().filter(item => item.categories === '道具箱'));
-
-
-
-// 固定ヘッダー（固定しない場合は = 0）
-headerHeight = 8;
-// イージング関数（easeOutQuart）
-function scrollToPos(position) {
-	const startPos = window.scrollY;
-	const distance = Math.min(position - startPos, document.documentElement.scrollHeight - window.innerHeight - startPos);
-	const duration = 500; // スクロールにかかる時間（ミリ秒）
-	let startTime;
-	function easeInOutCubic(x) {
-		return x < 0.5 ? 4 * x * x * x : 1 - Math.pow(-2 * x + 2, 3) / 2;
-	}
-
-	function animation(currentTime) {
-		if (startTime === undefined) {
-			startTime = currentTime;
-		}
-		const timeElapsed = currentTime - startTime;
-		const x = timeElapsed / duration;
-		const scrollPos = startPos + distance * easeInOutCubic(x);
-		window.scrollTo(0, scrollPos);
-		if (timeElapsed < duration) {
-			requestAnimationFrame(animation);
-		} else {
-			window.scrollTo(0, position);
-		}
-	}
-	requestAnimationFrame(animation);
-}
-// 遅延読み込み解除
-function removeLazyLoad() {
-	const targets = document.querySelectorAll('[data-src]');
-	for (const target of targets) {
-		target.setAttribute('src', target.getAttribute('data-src'));
-		target.addEventListener('load', () => {
-			target.removeAttribute('data-src');
-		});
-	}
-}
-// ページ内のスムーススクロール
-for (const link of document.querySelectorAll('a[href*="#"]')) {
-	link.addEventListener('click', (e) => {
-		const hash = e.currentTarget.hash;
-		const target = document.getElementById(hash.slice(1));
-
-		// ページトップへ("#"と"#top"）
-		if (!hash || hash === '#top') {
-			e.preventDefault();
-			scrollToPos(1); // iOSのChromeで固定ヘッダーが動くバグがあるため0ではなく1に
-
-		// アンカーへ
-		} else if (target) {
-			e.preventDefault();
-			removeLazyLoad();
-			const position = target.getBoundingClientRect().top + window.scrollY - headerHeight;
-			scrollToPos(position);
-
-			// URLにハッシュを含める
-			history.pushState(null, '', hash);
-		}
-	});
-};
-// 別ページ遷移後のスムーススクロール
-const urlHash = window.location.hash;
-if (urlHash) {
-	const target = document.getElementById(urlHash.slice(1));
-	if (target) {
-		// ページトップから開始（ブラウザ差異を考慮して併用）
-		history.replaceState(null, '', window.location.pathname);
-		window.scrollTo(0, 0);
-
-		removeLazyLoad();
-		window.addEventListener("load", () => {
-			const position = target.getBoundingClientRect().top + window.scrollY - headerHeight;
-			scrollToPos(position);
-
-			// ハッシュを再設定
-			history.replaceState(null, '', window.location.pathname + urlHash);
-		});
-	}
-};
